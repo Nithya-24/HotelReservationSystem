43b5d95381a7ffa8599f0c5e7e8ef10fe9beaa7f
Nithya-24 <nithyaramadoz@gmail.com>
2022-03-22 23:53:56 +0530

    Test case for finding the Cheapest Hotel is done

--- src/test/java/com/HotelReservationTest.java
+++ src/test/java/com/HotelReservationTest.java
@@ -4,13 +4,28 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class HotelReservationTest {
-
-	@Test
-			public void givenHotelDetails_WhenCreated_ShouldReturnTrue() {
-			HotelReservation hotelReservation = new HotelReservation();
-			boolean result = hotelReservation.addHotel();
-			assertEquals(true, result);
-		}
+	/**
+	 * JUnit Testing purpose
+	 */
 	
+	
+	@Test
+	public void test() {
 
+		HotelReservation.addHotel("Lakewood", 110);
+		HotelReservation.addHotel("Bridgewood", 160);
+		HotelReservation.addHotel("Ridgewood", 220);
+		assertEquals(3, HotelReservation.countNoOfHotels());
+	}
+
+	
+	@Test
+	public void givenDetailsOf3Hotels_InAGivenDataRage_shouldReturnCheapesthotel() {
+
+		HotelReservation.addHotel("Lakewood", 110);
+		HotelReservation.addHotel("Bridgewood", 160);
+		HotelReservation.addHotel("Ridgewood", 220);
+		String cheapestHotelInfo = HotelReservation.findCheapestHotel("10-09-2020", "11-09-2020");
+		assertEquals("Lakewood Total Cost: $110", cheapestHotelInfo);
+	}
 }
